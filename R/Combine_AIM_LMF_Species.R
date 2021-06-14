dcdc643074597b9eb528cdfe3e40aeb68d4ff3d4
nstauffer <ngstauffer@gmail.com>
2021-06-14 12:58:11 -0700

    Reformat

--- R/Combine_AIM_LMF_Species.R
+++ R/Combine_AIM_LMF_Species.R
@@ -1,22 +1,28 @@
-Combine_AIM_LMF_Species <- function(TerrADat_Path, Internal){
+Combine_AIM_LMF_Species <- function(TerrADat_Path,
+                                    Internal){
   if(!Internal){
-    TerrADatSpecies <- sf::st_read(dsn = TerrADat_Path , layer = "TerrADatSpeciesIndicators")
-    LMFSpecies <- sf::st_read(dsn = TerrADat_Path , layer = "LMFSpeciesIndicators")
+    TerrADatSpecies <- sf::st_read(dsn = TerrADat_Path,
+                                   layer = "TerrADatSpeciesIndicators")
+    LMFSpecies <- sf::st_read(dsn = TerrADat_Path,
+                              layer = "LMFSpeciesIndicators")
     TerrADatSpecies <- as.data.frame(TerrADatSpecies)
-    TerrADatSpecies <- dplyr::select(TerrADatSpecies, -Shape)
+    TerrADatSpecies <- dplyr::select(TerrADatSpecies,
+                                     -Shape)
     LMFSpecies <- as.data.frame(LMFSpecies)
-    LMFSpecies <- dplyr::select(LMFSpecies, -Shape)}
-
+    LMFSpecies <- dplyr::select(LMFSpecies,
+                                -Shape)}
+  
   if(Internal){
     TerrADatSpecies <- TerrADatSpecies
     LMFSpecies <- LMFSpecies
     SpeciesList <- SpeciesList
   }
-
-  LMFSpecies[setdiff(names(TerrADatSpecies) , names(LMFSpecies))] <- NA
+  
+  LMFSpecies[setdiff(names(TerrADatSpecies), names(LMFSpecies))] <- NA
   TerrADatSpecies[setdiff(names(LMFSpecies), names(TerrADatSpecies))] <- NA
-  output <- rbind(TerrADatSpecies , LMFSpecies)
-
+  output <- rbind(TerrADatSpecies,
+                  LMFSpecies)
+  
   return(output)
 }
 
